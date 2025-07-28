7059b689c0162d53b5a4884eb4826fe041040378
utku-fe <utkutekin86@gmail.com>
2025-07-28 18:01:30 +0300

    change main content style

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const main = document.querySelector("main");
 const footer = document.querySelector("footer");
 
 // dark theme
-toogleTheme.addEventListener("click", () => {
-  if (header.classList.contains("dark")) {
-    header.classList.remove("dark");
-    main.classList.remove("dark");
-    footer.classList.remove("dark");
-  } else {
-    header.classList.add("dark");
-    main.classList.add("dark");
-    footer.classList.add("dark");
-  }
-});
+// toogleTheme.addEventListener("click", () => {
+//   if (header.classList.contains("dark")) {
+//     header.classList.remove("dark");
+//     main.classList.remove("dark");
+//     footer.classList.remove("dark");
+//   } else {
+//     header.classList.add("dark");
+//     main.classList.add("dark");
+//     footer.classList.add("dark");
+//   }
+// });
